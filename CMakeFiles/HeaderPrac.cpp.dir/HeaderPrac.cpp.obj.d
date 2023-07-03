90ae0d3f9865d01d3171a15ced855e497eaf677c
Imsyp <tkddud386@gmail.com>
2023-07-03 14:08:20 +0900

    HeaderPrac

---
+++ CMakeFiles/HeaderPrac.cpp.dir/HeaderPrac.cpp.obj.d
@@ -0,0 +1,3 @@
+CMakeFiles/HeaderPrac.cpp.dir/HeaderPrac.cpp.obj: \
+ C:\Users\pc\Cpp_project\HeaderPrac.cpp\HeaderPrac.cpp \
+ C:\Users\pc\Cpp_project\HeaderPrac.cpp\head.h
---
+++ CMakeFiles/HeaderPrac.cpp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for HeaderPrac.cpp.
+# This may be replaced when dependencies are built.
